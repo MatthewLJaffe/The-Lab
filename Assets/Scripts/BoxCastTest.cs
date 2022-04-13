c74536901634bfc399c1dea79ed2d001387aecad
Matthew Jaffe <39813295+MatthewLJaffe@users.noreply.github.com>
2022-04-13 14:15:55 -0400

    Changed heal sound

---
+++ Assets/Scripts/BoxCastTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCastTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var hit = Physics2D.BoxCast(new Vector2(0, -8f), new Vector2(2, 2), 0, Vector2.zero, 0,
+            LayerMask.GetMask("Default"));
+        if (hit)
+            Debug.Log(hit.transform.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MiniMapScripts/AssignMiniMapCamera.cs
@@ -0,0 +1,23 @@
+﻿using CameraScripts;
+using PlayerScripts;
+using UnityEngine;
+
+namespace MiniMapScripts
+{
+    public class AssignMiniMapCamera : MonoBehaviour
+    {
+        private Camera _miniMapCamera;
+        private Canvas _canvas;
+
+        private void Awake()
+        {
+            _canvas = GetComponent<Canvas>();
+        }
+
+        private void Start()
+        {
+            _miniMapCamera = PlayerFind.instance.playerInstance.GetComponentInChildren<CameraCoordinator>().miniMapCamera;
+            _canvas.worldCamera = _miniMapCamera;
+        }
+    }
+}
